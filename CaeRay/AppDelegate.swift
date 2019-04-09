42de2629db54e7d3bbdadf1845931571305dd129
Caelin Jackson-King <caelink@gmail.com>
2019-04-09 01:56:40 -0400

    Initial Commit

---
+++ CaeRay/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  CaeRay
+//
+//  Created by Caelin Jackson-King on 2019-04-09.
+//  Copyright © 2019 Caelin Jackson-King. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
